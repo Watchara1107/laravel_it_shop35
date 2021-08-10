4fffed68e2556c5b5b6a05cbbb22ec99c33517b2
Watchara Ketkaew <wk996110@gmailcom>
2021-08-10 10:39:58 +0700

    update ครั้งที่ 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,10 @@
                     @endif
                     <p><strong>Name :</strong> {{ Auth::user()->name }}</p>
                     <p><strong>Email :</strong> {{ Auth::user()->email }}</p>
+                    <p><strong>Phone :</strong> {{ Auth::user()->phone }}</p>
+                    <p><strong>Address :</strong> {{ Auth::user()->address }}</p>
+                    <a href="" class="btn btn-primary">Product Management</a>
+                    <a href="" class="btn btn-success">Home</a>
                 </div>
             </div>
         </div>
